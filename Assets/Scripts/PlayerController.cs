462b6b1ee155ad05f1290aad08c3972df6a1a3a1
PedroAraujo004 <pedroalucena04@gmail.com>
2026-01-23 16:25:09 -0300

    feat (Player): implement basic player movement with speed check

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     public int health= 100;
     public float speed= 5.5f;
     public string PlayerName= "Pedro";
+    
+    private Vector2 movement;
+
     void Start()
     {
         
@@ -15,6 +18,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    if(speed > 0)
+    {
+       float input= Input.GetAxis("Horizontal");
+       float input2= Input.GetAxis("Vertical");
+
+       movement.x= input * speed * Time.deltaTime;
+       movement.y= input2 * speed * Time.deltaTime;
+       
+       transform.Translate(movement);
+    }
     }
 }
