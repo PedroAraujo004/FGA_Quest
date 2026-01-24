562a5dd3fd2ad8488ec039e64b54f38f041121f1
PedroAraujo004 <pedroalucena04@gmail.com>
2026-01-24 03:12:58 -0300

    Feature: Adds 4-directional walking animation and fixes movement script.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int health= 100;
-    public float speed= 5.5f;
-    public string PlayerName= "Pedro";
+    public int health = 100;
+    public float speed = 5.5f;
+    public string PlayerName = "Pedro";
     
+    // <--- 1. NÃO ESQUEÇA DE ARRASTAR O BONECO PARA CA NO INSPECTOR
+    public Animator animator; 
+
     private Vector2 movement;
 
     void Start()
@@ -15,18 +18,35 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-    if(speed > 0)
-    {
-       float input= Input.GetAxis("Horizontal");
-       float input2= Input.GetAxis("Vertical");
+        if(speed > 0)
+        {
+            // O seu usa GetAxis (com inércia/suavização)
+            float input = Input.GetAxis("Horizontal");
+            float input2 = Input.GetAxis("Vertical");
 
-       movement.x= input * speed * Time.deltaTime;
-       movement.y= input2 * speed * Time.deltaTime;
-       
-       transform.Translate(movement);
-    }
+            // Lógica de Movimento (Mantive a sua)
+            movement.x = input * speed * Time.deltaTime;
+            movement.y = input2 * speed * Time.deltaTime;
+            
+            transform.Translate(movement);
+
+            // <--- 2. LÓGICA DA ANIMAÇÃO ADICIONADA AQUI
+            // Verificamos se "input" ou "input2" são diferentes de zero (se tem alguém apertando botão)
+            if (input != 0 || input2 != 0)
+            {
+                animator.SetFloat("MoveX", input);
+                animator.SetFloat("MoveY", input2);
+                
+                // Opcional: Avisar o animator que estamos andando (se tiver transição Idle/Walk)
+                animator.SetBool("IsMoving", true);
+            }
+            else
+            {
+                // Opcional: Avisar que parou
+                animator.SetBool("IsMoving", false);
+            }
+        }
     }
 }
