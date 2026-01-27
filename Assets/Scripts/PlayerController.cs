23e5c6d760a8b2c8c0ee77289caac578c21971c8
PedroAraujo004 <pedroalucena04@gmail.com>
2026-01-26 23:25:44 -0300

    Refactoring: Modularizing movement and correcting Start

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,45 +8,47 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.5f;
     public string PlayerName = "Pedro";
     
-    // <--- 1. NÃO ESQUEÇA DE ARRASTAR O BONECO PARA CA NO INSPECTOR
     public Animator animator; 
 
     private Vector2 movement;
 
     void Start()
     {
-        
+        if (animator == null)
+            animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if(speed > 0)
-        {
-            // O seu usa GetAxis (com inércia/suavização)
+        Move();
+    }
+
+    void Move()
+    {
             float input = Input.GetAxis("Horizontal");
             float input2 = Input.GetAxis("Vertical");
 
-            // Lógica de Movimento (Mantive a sua)
             movement.x = input * speed * Time.deltaTime;
             movement.y = input2 * speed * Time.deltaTime;
             
             transform.Translate(movement);
 
-            // <--- 2. LÓGICA DA ANIMAÇÃO ADICIONADA AQUI
-            // Verificamos se "input" ou "input2" são diferentes de zero (se tem alguém apertando botão)
             if (input != 0 || input2 != 0)
             {
                 animator.SetFloat("MoveX", input);
                 animator.SetFloat("MoveY", input2);
                 
-                // Opcional: Avisar o animator que estamos andando (se tiver transição Idle/Walk)
                 animator.SetBool("IsMoving", true);
             }
             else
             {
-                // Opcional: Avisar que parou
                 animator.SetBool("IsMoving", false);
-            }
-        }
+            } 
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health= health - amount;
+        Debug.Log("Vida atual: " + health);
     }
 }
