accdde6fa416d248a9241a095f033ba462fbb7f9
PedroAraujo004 <pedroalucena04@gmail.com>
2026-01-30 13:43:56 -0300

    feat: changed enemy spawning system using arrays

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,35 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
-{
-    [SerializeField] GameObject enemyPrefab;
-
-    [SerializeField] float spawnInterval= 0.5f;
-
-    [SerializeField] int maxEnemies= 5;
-    // Start is called before the first frame update
-    void Start()
+    public class EnemySpawner : MonoBehaviour
     {
-        StartCoroutine(spawnEnemy());
-    }
+        [SerializeField] GameObject enemyPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        [SerializeField] float spawnInterval= 0.5f;
 
-    private IEnumerator spawnEnemy()
-    {
-        int enemySpawned= 0;
+        [SerializeField] int maxEnemies= 5;
 
-        while(enemySpawned < maxEnemies)
+        public GameObject[] enemies;
+
+        // Start is called before the first frame update
+        void Start()
         {
-        yield return new WaitForSeconds(spawnInterval);
-        GameObject newEnemy= Instantiate(enemyPrefab, new Vector2(Random.Range(-5,5), Random.Range(-5, 5)), Quaternion.identity);
-        enemySpawned++;
+            enemies= new GameObject[maxEnemies];
+
+            StartCoroutine(spawnEnemy());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        private IEnumerator spawnEnemy()
+        {
+            
+            for(int i= 0; i < maxEnemies; i++)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+            GameObject newEnemy= Instantiate(enemyPrefab, new Vector2(Random.Range(-5,5), Random.Range(-5, 5)), Quaternion.identity);
+            enemies[i]= newEnemy;
+
+            newEnemy.name= "Enemy: " +i;
+        }
+            
         }
     }
-}
